354cd32d8bc72ff33cf31636a3581cfe2a11f09b
Dani Cea <daniel.cea@contractor.ca.com>
2015-03-10 16:07:27 +0100

    Erased keys + ES url from config

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 # Twitter settings
 twitter = {
-	"consumer_key": 'QULl7GNb5JKzwKznevMZmQ',
-	"consumer_secret": 'tKbUP8DqcjjYgl9Ih0sIt6TNPebTNdgkCoWJ0qfW9Sc',
-	"access_token": '2353070090-LjkVD41lVcR8dqNb2gg29IIFqpySSYHmgioEB0D',
-	"access_token_secret": 'VoZS0ENHbtgNhVTvvWgaMAp4Nsc2grcdhXGkP91VSSML5'	
+	"consumer_key": 'INSERT_CONSUMER_KEY_HERE',
+	"consumer_secret": 'INSERT_CONSUMER_SECRET_HERE',
+	"access_token": 'INSERT_ACCESS_TOKEN_HERE',
+	"access_token_secret": 'INSERT_ACCESS_TOKEN_SECRET_HERE'	
 }
 
 # ElasticSearch settings
 es = {
-	"server": 'http://minerva.bsc.es:8089',
+	"server": 'INSERT_ELASTICSEARCH_URL_HERE',
 	"index_settings": {
 	    "mappings" : {
 		    "tweet": {
